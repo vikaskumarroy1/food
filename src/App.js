ed5343a853a6563deb3530a479d2c8891ebbebf6
classwwork <vikasroy1307@gmail.com>
2023-12-20 11:21:04 +0530

    vikas

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Button from './Components/Button';
+import Hero from './Components/Hero';
+import Card from './Components/Card';
+
+
+
+function App() {
+  return (
+    <div className="App">
+         <Header/>
+        <Hero/>
+        <Card/>
+        <Card/>
+        <Card/>
+        
+         
+        
+    </div>
+  );
+}
+
+export default App;
